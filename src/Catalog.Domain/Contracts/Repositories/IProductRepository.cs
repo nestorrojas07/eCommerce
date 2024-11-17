8a77b6b4f55338ceb0750ba48571362370f2966f
Nestor Rojas <nestor.rojas@masiv.com>
2024-11-17 11:05:05 -0500

    configuring domain

---
+++ src/Catalog.Domain/Contracts/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Models;
+namespace Catalog.Domain.Contracts.Repositories;
+public interface IProductRepository
+{
+    public Task<Product> SaveAsync(Product product, CancellationToken cancellationToken = default);
+    public Task<Product?> GetByIdAsync(long id, CancellationToken cancellationToken = default);
+    public Task<ICollection<Product>> GetAsync(int pageNumber = 0, int pageSize = 10, CancellationToken cancellationToken = default);
+
+    public Task<bool> UpdateAsync(Product product, CancellationToken cancellationToken = default); 
+    public Task<bool> DeleteAsync(long id, CancellationToken cancellationToken = default);
+}
---
+++ src/Catalog.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }= DateTime.Now;
+        public bool IsActive { get; set; } = true;
+    }
+
+}
