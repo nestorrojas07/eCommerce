8b997e3ef883428d11df62c0cb180d2d38bf5831
Nestor Rojas <nestorrojas07@gmail.com>
2024-11-18 10:15:25 -0500

    configuring models

---
+++ src/Order.Domain/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.Models;
+
+public class OrderDetail
+{
+    public long Id { get; set; }
+    public long OrderId { get; set; }
+    public long ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int Quatity { get; set; }
+    public int UnitValue { get; set; }
+    public float Total { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
